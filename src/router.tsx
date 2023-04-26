1127025aa023784151004ce20415793d3a25dc06
Дмитрий Коровин <dmitrijkorovin@Dmitrijs-MacBook-Air.local>
2023-04-27 00:42:29 +0300

    basename param

--- src/router.tsx
+++ src/router.tsx
@@ -1,20 +1,15 @@
 import { createBrowserRouter } from "react-router-dom";
-// import { HelloWorld } from "./components/HelloWorld/HelloWorld";
-import { AddSpent } from "./pages/AddSpent/AddSpent";
 import { History } from "./pages/History/History";
-import { Stats } from "./pages/Stats/Stats";
+import config from "./config.json";
 
-export const router = createBrowserRouter([
+export const router = createBrowserRouter(
+  [
+    {
+      path: "/",
+      element: <History />,
+    },
+  ],
   {
-    path: "/",
-    element: <History/>,
-  },
-  {
-    path: "/add",
-    element: <AddSpent/>,
-  },
-  {
-    path: "/stats",
-    element: <Stats/>,
-  },
-]);
+    basename: config.BASENAME,
+  }
+);
